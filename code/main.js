31768570db2046a9bc33c4385f186a1471d29380
shenhaitao669 <shenhaitao669@163.com>
2023-05-18 22:49:10 +0800

    m

--- code/main.js
+++ code/main.js
@@ -16,13 +16,18 @@ const interactive = () => {
 const compute = () => {
 };
 
+const ground1 = new Animation(192, 175, 192, 175, "res/fire_ground.png");
+const ground2 = new Animation(196, 132, 196, 132, "res/grass ground.png");
+const ground3 = new Animation(71, 142, 71, 142, "res/tree1.png");
+const ground4 = new Animation(143, 104, 143, 104, "res/6.png");
+
 const animation1 = new Animation(128 / 2, 128 / 2, 128, 128, "res/blue_fire.png");
 const animation2 = new Animation(128 / 2, 128 / 2, 128, 128, "res/fire_attack.png");
 const animation3 = new Animation(128 / 2, 128 / 2, 128, 128, "res/sword_attack.png");
 const animation4 = new Animation(256 / 2, 512 / 2, 256, 512, "res/fire_burn.png");
 const animation6 = new Animation(128, 256, 128, 256, "res/fire_burn2.png");
 const animation7 = new Animation(144, 192, 144, 192, "res/ice_attack.png");
-const animation8 = new Animation(64, 104, 64, 110, "res/person_move.png");
+const animation8 = new Animation(64, 112, 64, 112, "res/person_move.png");
 const onLoopEnd = [
     () => animation8.setFrameRange(1, 1 + 6).loop(parseInt(Math.random() * 10) + 1).onLoopEnded(() => onLoopEnd[1]()),
     () => animation8.setFrameRange(8, 8 + 6).loop(parseInt(Math.random() * 10) + 1).onLoopEnded(() => onLoopEnd[0]())
@@ -30,8 +35,13 @@ const onLoopEnd = [
 animation8.onLoaded(() => onLoopEnd[0]());
 
 const render = () => {
-    pen.fillStyle = "#000";
+    pen.fillStyle = "#fff";
     pen.fillRect(0, 0, 800, 600);
+
+    ground1.play(pen, 0, 0);
+    ground2.play(pen, 192, 0);
+    ground3.play(pen, 388, 0);
+    ground4.play(pen, 459, 142)
 
     animation1.play(pen, 0, 0);
     animation2.play(pen, 0, 64);
